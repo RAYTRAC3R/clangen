b2868934170ccbcdd2375db482a889012ecef8cc
RAYTRAC3R <samipiplup@gmail.com>
2024-02-19 08:39:21 -0600

    Fix rename crash, add basic naming system
    Game previously crashed when trying to rename a pre-existing cat, I fixed it by swapping out the variables used for pelt color and eye color in the naming system.
    I also added a few names for red and orange cats.

--- scripts/game_structure/windows.py
+++ scripts/game_structure/windows.py
@@ -416,8 +416,8 @@ class ChangeCatName(UIWindow):
                 self.prefix_entry_box.set_text(Name(self.the_cat.status,
                                                     None,
                                                     use_suffix,
-                                                    self.the_cat.pelt.colour,
-                                                    self.the_cat.pelt.eye_colour,
+                                                    self.the_cat.pelt.name,
+                                                    self.the_cat.pelt.eye_tint,
                                                     self.the_cat.pelt.name,
                                                     self.the_cat.pelt.tortiepattern).prefix)
             elif event.ui_element == self.random_suffix:
