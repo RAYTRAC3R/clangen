509f8ae12259cf8b0c744375ad795f05ee27586d
RAYTRAC3R <samipiplup@gmail.com>
2024-02-19 17:16:48 -0600

    Added pony races!

--- scripts/screens/ProfileScreen.py
+++ scripts/screens/ProfileScreen.py
@@ -694,7 +694,7 @@ class ProfileScreen(Screens):
         output += "\n"
 
         # PELT LENGTH
-        output += 'fur length: ' + the_cat.pelt.length
+        output += 'race: ' + the_cat.pelt.race.lower()
         # NEWLINE ----------
 
         # ACCESSORY
