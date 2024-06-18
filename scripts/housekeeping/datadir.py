9e07a0eac58af5c9f0517f798dfd5ed4c2d64ca0
RAYTRAC3R <samipiplup@gmail.com>
2024-06-17 20:18:37 -0500

    Update datadir.py

--- scripts/housekeeping/datadir.py
+++ scripts/housekeeping/datadir.py
@@ -32,8 +32,8 @@ def get_data_dir():
     from platformdirs import user_data_dir
 
     if get_version_info().is_dev():
-        return user_data_dir('PrismGen', 'ClanGen')
-    return user_data_dir('PrismGen', 'ClanGen')
+        return user_data_dir('MLGen', 'ClanGen')
+    return user_data_dir('MLGen', 'ClanGen')
 
 
 def get_log_dir():
